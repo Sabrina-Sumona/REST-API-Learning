09fdb396ea409e1020599cdae84075fb44086825
Sabrina Sumona <snsbauet04@gmail.com>
2021-04-20 13:57:26 +0600

    setting up our own REST API

---
+++ script.js
@@ -0,0 +1,22 @@
+// HTTP (HyperText Transfer Protocol) is used to connect the Web servers with the browser
+// This uses request & response messages for an URL(Uniform Resource Locator)
+// URL exapmle: www.abcd.com
+// resposce codes: 404 -> not found, 403 -> forbidden, 200 -> OK
+
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+
+// https://websniffer.cc/
+
+// https://restfulapi.net/
+
+// REST (REpresentational State Transfer) API (Application Programming Interface) is used to allows communication between two applications
+// It uses similar strategy like HTTP request & response to access the URI(Uniform Resource Indicator)
+// URI exapmle: www.abcd.com/letters
+// www.abcd.com/letters/1
+
+// REST API Examples
+// https://jsonplaceholder.typicode.com/
+// https://docs.github.com/en/
+
